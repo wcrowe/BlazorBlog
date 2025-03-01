7c481d08db905496e6c066daa4237f902da86c98
wcrowe <crowe.will@gmail.com>
2025-03-01 15:43:23 -0500

    Update Edtior

--- BlazorBlog.WebUI.Server/Components/Pages/ArticleModel.cs
+++ BlazorBlog.WebUI.Server/Components/Pages/ArticleModel.cs
@@ -2,6 +2,7 @@
 {
     public class ArticleModel
     {
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Content { get; set; }
         public DateTime DatePublished { get; set; } = DateTime.Now;
