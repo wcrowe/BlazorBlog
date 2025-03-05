45fc7487bd22ca9e172e024f923ffbb26bce9c11
wcrowe <crowe.will@gmail.com>
2025-03-05 03:24:46 -0500

    Updaes

---
+++ BlazorBlog.WebUI.Server/Features/Users/LoginUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBlog.WebUI.Server.Features.Users
+{
+    public class LoginUserModel
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
