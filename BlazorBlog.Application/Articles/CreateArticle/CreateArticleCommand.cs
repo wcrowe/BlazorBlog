a0567e2d2286316966ef88948abf93cc91636dea
wcrowe <crowe.will@gmail.com>
2025-03-07 20:15:40 -0500

    Edit dosn

--- BlazorBlog.Application/Articles/CreateArticle/CreateArticleCommand.cs
+++ BlazorBlog.Application/Articles/CreateArticle/CreateArticleCommand.cs
@@ -2,6 +2,7 @@
 
 public class CreateArticleCommand : ICommnd<ArticleResponse>
 {
+
     public required string Title { get; set; }
     public string? Content { get; set; }
     public DateTime DatePublished { get; set; } = DateTime.UtcNow;
--- BlazorBlog.WebUI.Server/Features/Articles/ArticleModel.cs
+++ BlazorBlog.WebUI.Server/Features/Articles/ArticleModel.cs
@@ -7,5 +7,7 @@
         public string? Content { get; set; }
         public DateTime DatePublished { get; set; } = DateTime.Now;
         public bool IsPublished { get; set; } = false;
+        public string? UserName { get; set; } = null;
+
     }
 }
