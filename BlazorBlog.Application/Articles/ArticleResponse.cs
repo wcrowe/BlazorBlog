a224677e511315d6aded6aec9d44224878272167
wcrowe <crowe.will@gmail.com>
2025-03-12 20:04:41 -0400

    ArticleResponse to record structure

--- BlazorBlog.Application/Articles/ArticleResponse.cs
+++ BlazorBlog.Application/Articles/ArticleResponse.cs
@@ -1,14 +1,17 @@
 ﻿namespace BlazorBlog.Application.Articles;
-public record ArticleResponse
-{
-    public int Id { get; set; }
-    public required string Title { get; set; }
-    public string? Content { get; set; }
-    public DateTime DatePublished { get; set; }
-    public bool IsPublished { get; set; }
-    public string? UserId { get; set; } = null;
-    public string? UserName { get; set; } = null;
-}
+public record struct ArticleResponse(int Id, string Title, 
+    string? Content, DateTime DatePublished, bool IsPublished, 
+    string UserId, string UserName, bool CanEdit);
+
+//{
+//    public int Id { get; set; }
+//    public required string Title { get; set; }
+//    public string? Content { get; set; }
+//    public DateTime DatePublished { get; set; }
+//    public bool IsPublished { get; set; }
+//    public string? UserId { get; set; } = null;
+//    public string? UserName { get; set; } = null;
+//}
 
 
 
