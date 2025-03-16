9b0cfd3b634912033f65aef60ef4deecab7b15aa
wcrowe <crowe.will@gmail.com>
2025-03-16 10:00:02 -0400

    Client Project Added

--- BlazorBlog.WebUI.Client/Program.cs
+++ BlazorBlog.WebUI.Client/Program.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorBlog.WebUI.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new HttpClient {
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
 
 await builder.Build().RunAsync();
